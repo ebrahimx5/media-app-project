c0593e75befb7eba40dfa872c0aa2cc307f53c56
ebrahimx5 <khelanx5@gmail.com>
2026-01-27 18:29:13 +0300

    add update routes

--- controllers/film.routes.js
+++ controllers/film.routes.js
@@ -4,46 +4,111 @@ const Film = require('../models/Film')
 
 
 
-router.get('/create' , (req,res)=>{
-    
-
-const genres = [
-'Action',
-
-'Comedy',
-
-'Drama',
-
-'Fantasy',
-
-'Horror',
-
-'Mystery',
-
-'Romance',
-
-'Science Fiction',
-
-'Thriller',
-
-'Western',
-
-'Musical',
-
-'Animation',]
-
-const ageRating = ['G', 'PG', '13+', '15+', '18+']
+router.get('/create', (req, res) => {
 
 
-    res.render('create-film.ejs',{genres : genres, ageRating: ageRating})
+    const genres = [
+        'Action',
+
+        'Comedy',
+
+        'Drama',
+
+        'Fantasy',
+
+        'Horror',
+
+        'Mystery',
+
+        'Romance',
+
+        'Science Fiction',
+
+        'Thriller',
+
+        'Western',
+
+        'Musical',
+
+        'Animation',]
+
+    const ageRating = ['G', 'PG', '13+', '15+', '18+']
+
+
+    res.render('create-film.ejs', { genres: genres, ageRating: ageRating })
 })
 
-router.post('/create', async(req, res)=>{
+router.post('/create', async (req, res) => {
     req.body.userCard = req.session.user._id
     createdFilm = await Film.create(req.body)
     console.log(createdFilm)
     res.redirect('/film/create')
 })
+
+router.get('/edit/:id', async (req, res) => {
+
+    const genres = [
+        'Action',
+
+        'Comedy',
+
+        'Drama',
+
+        'Fantasy',
+
+        'Horror',
+
+        'Mystery',
+
+        'Romance',
+
+        'Science Fiction',
+
+        'Thriller',
+
+        'Western',
+
+        'Musical',
+
+        'Animation',]
+
+    const ageRating = ['G', 'PG', '13+', '15+', '18+']
+
+    const foundFilm = await Film.findById(req.params.id)
+    res.render('edit-film.ejs', { foundFilm: foundFilm, ageRating : ageRating, genres : genres })
+})
+
+
+
+router.post('/edit/:id', async (req,res)=>{
+
+    editFilm = await Film.findByIdAndUpdate(req.params.id, req.body);
+    console.log(editFilm)
+    res.redirect('/')
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;

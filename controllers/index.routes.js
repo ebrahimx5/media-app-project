13995f04b24d7e5b9997f96d23dc8017be8f8c61
Ebrahim <khelanx5@gmail.com>
2026-02-01 17:33:29 +0300

    more styling to all website

--- controllers/index.routes.js
+++ controllers/index.routes.js
@@ -6,16 +6,24 @@ router.get('/', async (req,res) => {
   try {
     let allFilm;
     let allSeries;
+    let filmCount = 0;
+    let seriesCount = 0;
+    let totalCards = 0;
+    let username = null;
 
     if(req.session.user) {        
       allFilm = await Film.find({ userCard: req.session.user._id }).populate('userCard');
       allSeries = await Series.find({ userCard: req.session.user._id }).populate('userCard');
-    } else {
+      filmCount = allFilm.length;
+      seriesCount = allSeries.length;
+      totalCards = filmCount + seriesCount;
+      username = req.session.user.username;
+    } else {        
       allFilm = await Film.find().populate('userCard');
       allSeries = await Series.find().populate('userCard');
     }
 
-    res.render('homepage.ejs', { allFilm: allFilm, allSeries: allSeries });
+    res.render('homepage.ejs', { allFilm : allFilm,  allSeries: allSeries,username: username, filmCount: filmCount,seriesCount: seriesCount, totalCards: totalCards});
   } catch (error) {
     console.log("Error in GET /:", error);
   }
@@ -27,6 +35,10 @@ router.get('/about', (req,res) =>{
 
 router.get('/services', (req,res) =>{
   res.render('services.ejs');
+});
+
+router.get('/contact', (req,res) =>{
+  res.render('contact.ejs');
 });
 
 router.get('/community', async (_, res) => {
